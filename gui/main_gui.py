0e88232dfc66d0cb87cf0f1361285a7384beb0f3
Paloma Sette <palomafl.sette@gmail.com>
2025-05-28 18:37:30 -0300

    Depth Adjust

--- gui/main_gui.py
+++ gui/main_gui.py
@@ -1,14 +1,24 @@
 """
 script principal da interface
 """
-
+import cv2
+import sys
+import os
+sys.path.append(os.path.abspath(".."))
 import webbrowser
+import threading
 from tkinter import Menu, messagebox
 import customtkinter as ctk
 import tkinter as tk
 from PIL import Image, ImageTk
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import depthai as dai
+from vision.depth_stream import create_pipeline, filter_depth_range
+
+import numpy as np
+from PIL import Image, ImageTk
+import numpy as np
 
 ctk.set_appearance_mode("dark")
 ctk.set_default_color_theme("blue")
@@ -18,15 +28,16 @@ class RaiseGui(ctk.CTk):
     def __init__(self):
         super().__init__()
         
-        # <- Tamanho inicial da janela (largura x altura)
         self.geometry("1325x800")
         self.minsize(1000, 600)
-        
         self.title("RAISE - Robotic Acoustic Inspection with Surface Estimation")
         self.grid_columnconfigure((0, 1, 2), weight=1)
         self.grid_rowconfigure((0, 1, 2), weight=1)
         
-
+        # parametros de profundidade
+        self.min_depth = 100 # mm
+        self.max_depth = 10000 # mm
+        
         # scroller
         container = ctk.CTkFrame(self)
         container.pack(fill="both", expand=True)
@@ -62,15 +73,18 @@ class RaiseGui(ctk.CTk):
                             sticky="nsew", padx=10, pady=5)
         self.frame_top.grid_columnconfigure((0, 1, 2), weight=1)
 
-        self.depth_frame = self.create_section(
+        self.depth_frame, self.depth_canvas = self.create_section(
             self.frame_top, "Depth View", "(Imagem depth aqui)")
+
+
         self.depth_frame.grid(row=0, column=0, padx=5, pady=5, sticky="nsew")
 
-        self.rgb_frame = self.create_section(
+        self.rgb_frame, self.rgb_canvas = self.create_section(
             self.frame_top, "RGB View", "(Imagem RGB aqui)")
         self.rgb_frame.grid(row=0, column=1, padx=5, pady=5, sticky="nsew")
 
-        self.normals_frame = self.create_section(
+
+        self.normals_frame, self.normals_canvas = self.create_section(
             self.frame_top, "Normals / Profile", "(Imagem Normals aqui)")
         self.normals_frame.grid(row=0, column=2, padx=5, pady=5, sticky="nsew")
 
@@ -80,16 +94,16 @@ class RaiseGui(ctk.CTk):
                                sticky="nsew", padx=10, pady=5)
         self.frame_middle.grid_columnconfigure((0, 1, 2), weight=1)
 
-        self.winding_frame = self.create_section(
+        self.winding_frame, self.winding_canvas = self.create_section(
             self.frame_middle, "Winding", "(Imagem Winding aqui)")
         self.winding_frame.grid(row=0, column=0, padx=5, pady=5, sticky="nsew")
 
-        self.embedding_frame = self.create_section(
+        self.embedding_frame, self.embedding_canvas = self.create_section(
             self.frame_middle, "Embedding", "(Imagem Embedding aqui)")
         self.embedding_frame.grid(
             row=0, column=1, padx=5, pady=5, sticky="nsew")
 
-        self.rqa_frame = self.create_section(
+        self.rqa_frame, self.rqa_canvas = self.create_section(
             self.frame_middle, "RQA + Entropy", "(Imagem RQA aqui)")
         self.rqa_frame.grid(row=0, column=2, padx=5, pady=5, sticky="nsew")
 
@@ -114,6 +128,56 @@ class RaiseGui(ctk.CTk):
         self.debug_button = ctk.CTkButton(
             self.frame_bottom, text="Modo Debug", command=self.toggle_debug)
         self.debug_button.grid(row=0, column=3, padx=10, pady=10)
+        
+
+        # Sliders de profundidade
+        self.slider_min = ctk.CTkSlider(self.frame_bottom, from_=100, to=10000, number_of_steps=10,
+                                        command=self.update_min_depth, width=200)
+        self.slider_min.set(self.min_depth)
+        self.slider_min.grid(row=1, column=0, columnspan=2, padx=10, pady=5)
+
+        self.label_min = ctk.CTkLabel(
+            self.frame_bottom, text=f"Min Depth: {self.min_depth} mm")
+        self.label_min.grid(row=1, column=2, padx=5)
+
+        self.slider_max = ctk.CTkSlider(self.frame_bottom, from_=100, to=10000, number_of_steps=10,
+                                        command=self.update_max_depth, width=200)
+        self.slider_max.set(self.max_depth)
+        self.slider_max.grid(row=2, column=0, columnspan=2, padx=10, pady=5)
+
+        self.label_max = ctk.CTkLabel(
+            self.frame_bottom, text=f"Max Depth: {self.max_depth} mm")
+        self.label_max.grid(row=2, column=2, padx=5)
+        
+        
+    def update_min_depth(self, value):
+        self.min_depth = int(value)
+        self.label_min.configure(text=f"Min Depth: {self.min_depth} mm")
+
+    def update_max_depth(self, value):
+        self.max_depth = int(value)
+        self.label_max.configure(text=f"Max Depth: {self.max_depth} mm")
+
+
+    def render_profile_plot(self, depth_frame):
+        depth_line = depth_frame[240, :]  # linha central
+        
+        fig, ax = plt.subplots(figsize=(4.2, 2.5), dpi=100)
+        ax.plot(depth_line, color="blue")
+        ax.set_title("Perfil da Superfície")
+        ax.set_xlabel("Pixel")
+        ax.set_ylabel("Profundidade (u)")
+        ax.grid(True)
+
+        if hasattr(self, 'profile_canvas'):
+            self.profile_canvas.get_tk_widget().grid_forget()
+            self.profile_canvas.get_tk_widget().destroy()
+
+        self.profile_canvas = FigureCanvasTkAgg(fig, master=self.normals_canvas)
+        self.profile_canvas.draw()
+        self.profile_canvas.get_tk_widget().grid(row=0, column=0, sticky="nsew")
+
+
 
     def create_section(self, parent, title_text, placeholder_text):
         section_frame = ctk.CTkFrame(parent)
@@ -126,7 +190,7 @@ class RaiseGui(ctk.CTk):
         canvas = ctk.CTkLabel(
             section_frame, text=placeholder_text, width=420, height=240, text_color="gray")
         canvas.pack(pady=30)
-        return section_frame
+        return section_frame, canvas
 
     def open_readme(self):
         webbrowser.open(
@@ -139,7 +203,64 @@ class RaiseGui(ctk.CTk):
         )
 
     def start_system(self):
-        print("Sistema iniciado (placeholder)")
+        print("Sistema iniciado")
+        threading.Thread(target=self.start_camera_stream, daemon=True).start()
+
+    def updateArg(self, arg_name, arg_value, shouldUpdate=True):
+        setattr(self.confManager.args, arg_name, arg_value)
+        if shouldUpdate:
+            self.worker.signals.setDataSignal.emit(
+                    ["restartRequired", True])
+            print("CONFMANAGER ------->" + str(self.confManager.args))
+
+    def start_camera_stream(self):
+        pipeline = create_pipeline()
+        self.device = dai.Device(pipeline)
+        self.rgb_queue = self.device.getOutputQueue(name="rgb", maxSize=4, blocking=False)
+        self.depth_queue = self.device.getOutputQueue(name="depth", maxSize=4, blocking=False)
+
+        self.update_camera_frames()
+
+    def update_camera_frames(self):
+        # RGB
+        in_rgb = self.rgb_queue.tryGet()
+        if in_rgb:
+            rgb_frame = in_rgb.getCvFrame()
+            img = Image.fromarray(cv2.cvtColor(rgb_frame, cv2.COLOR_BGR2RGB))
+            imgtk = ImageTk.PhotoImage(image=img.resize((440, 350)))
+            #imgtk = ctk.CTkImage(Image.fromarray(cv2.cvtColor(rgb_frame, cv2.COLOR_BGR2RGB)), size=(390, 300))
+            self.rgb_canvas.configure(image=imgtk, text="")
+            self.rgb_canvas.image = imgtk
+
+        # Depth
+        in_depth = self.depth_queue.tryGet()
+        if in_depth:
+            depth_frame = in_depth.getFrame()
+            depth_frame = filter_depth_range(depth_frame)
+            depth_frame = depth_frame.astype(np.uint16)
+            
+            # self.render_profile_plot(depth_frame) # na label de Profile/Normals, vai atualizando o perfil
+            # depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_frame, alpha=0.1), cv2.COLORMAP_JET)
+
+            # Clipa e normaliza manualmente para 0–255
+            depth_clip = np.clip(depth_frame, self.min_depth, self.max_depth)
+            depth_norm = ((depth_clip - self.min_depth) /
+                          (self.max_depth - self.min_depth) * 255).astype(np.uint8)
+
+            # Aplica o colormap
+            depth_colormap = cv2.applyColorMap(depth_norm, cv2.COLORMAP_JET)
+
+            img = Image.fromarray(depth_colormap)
+            imgtk = ImageTk.PhotoImage(image=img.resize((440, 350)))
+            #imgtk = ctk.CTkImage(Image.fromarray(cv2.cvtColor(depth_colormap, cv2.COLOR_BGR2RGB)), size=(390, 300))
+            self.depth_canvas.configure(image=imgtk, text="")
+            self.depth_canvas.image = imgtk
+            
+
+
+
+        self.after(30, self.update_camera_frames)  # Atualiza a cada 30ms (~30FPS)
+
 
     def reset_robot(self):
         print("Robô resetado (placeholder)")
--- vision/depth_stream.py
+++ vision/depth_stream.py
@@ -1,3 +1,77 @@
-""" 
-Captura do depth map
 """
+Captura do depth map com controle de parâmetros.
+"""
+
+import depthai as dai
+import numpy as np
+from depthai_sdk import Record
+
+
+DEPTH_MIN = 100   
+DEPTH_MAX = 10000 
+
+
+"""
+Captura do depth map com parâmetros refinados
+"""
+
+
+def create_pipeline():
+    pipeline = dai.Pipeline()
+
+    # Câmera RGB
+    cam_rgb = pipeline.create(dai.node.ColorCamera)
+    cam_rgb.setPreviewSize(640, 480)
+    cam_rgb.setInterleaved(False)
+    cam_rgb.setFps(30)
+
+    # Câmeras mono
+    mono_left = pipeline.create(dai.node.MonoCamera)
+    mono_right = pipeline.create(dai.node.MonoCamera)
+    stereo = pipeline.create(dai.node.StereoDepth)
+
+    mono_left.setBoardSocket(dai.CameraBoardSocket.CAM_B)
+    mono_right.setBoardSocket(dai.CameraBoardSocket.CAM_C)
+    mono_left.setResolution(
+        dai.MonoCameraProperties.SensorResolution.THE_400_P)
+    mono_right.setResolution(
+        dai.MonoCameraProperties.SensorResolution.THE_400_P)
+
+    # Ligações
+    mono_left.out.link(stereo.left)
+    mono_right.out.link(stereo.right)
+
+    # --- Parâmetros adicionais para melhorar visualização da profundidade ---
+    stereo.setLeftRightCheck(True)
+    stereo.setExtendedDisparity(True)
+    stereo.setSubpixel(True)
+    stereo.setDepthAlign(dai.CameraBoardSocket.RGB)
+    stereo.initialConfig.setConfidenceThreshold(240)
+    stereo.initialConfig.setMedianFilter(dai.MedianFilter.KERNEL_7x7)
+    stereo.initialConfig.setBilateralFilterSigma(0)
+    stereo.initialConfig.setLeftRightCheckThreshold(10)
+
+    # Saídas
+    xout_rgb = pipeline.create(dai.node.XLinkOut)
+    xout_rgb.setStreamName("rgb")
+    cam_rgb.preview.link(xout_rgb.input)
+
+    xout_depth = pipeline.create(dai.node.XLinkOut)
+    xout_depth.setStreamName("depth")
+    stereo.setOutputDepth(True)
+
+    stereo.depth.link(xout_depth.input)
+
+    return pipeline
+
+
+
+# ---------- UTILIDADE OPCIONAL PARA APLICAR THRESHOLD ----------
+def filter_depth_range(depth_frame, min_depth=DEPTH_MIN, max_depth=DEPTH_MAX):
+    """
+    Filtra a imagem de profundidade para manter apenas valores dentro da faixa desejada.
+    Os pixels fora da faixa são zerados.
+    """
+    filtered = np.copy(depth_frame)
+    filtered[(filtered < min_depth) | (filtered > max_depth)] = 0
+    return filtered
